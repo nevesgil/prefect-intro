34fd3ef25d56d55a9382600bb3ab072004b30511
nevesgil <gilmarneves@outlook.com>
2022-04-22 21:04:08 -0300

    basic flow consuming from api

---
+++ demo.py
@@ -0,0 +1,67 @@
+import requests
+import json
+from collections import namedtuple
+from contextlib import closing
+import sqlite3
+
+from prefect import task, Flow
+
+
+## extract
+@task
+def get_complaint_data():
+    r = requests.get(
+        "https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/",
+        params={"size": 10},
+    )
+    response_json = json.loads(r.text)
+    """for now, let us use only part of the data"""
+    return response_json["hits"]["hits"]
+
+
+## transform
+@task
+def parse_complaint_data(raw):
+    complaints = []
+    Complaint = namedtuple(
+        "Complaint",
+        ["data_received", "state", "product", "company", "complaint_what_happened"],
+    )
+    for row in raw:
+        source = row.get("_source")
+        this_complaint = Complaint(
+            data_received=source.get("date_recieved"),
+            state=source.get("state"),
+            product=source.get("product"),
+            company=source.get("company"),
+            complaint_what_happened=source.get("complaint_what_happened"),
+        )
+        complaints.append(this_complaint)
+    return complaints
+
+
+## load
+@task
+def store_complaints(parsed):
+    create_script = "CREATE TABLE IF NOT EXISTS complaint (timestamp TEXT, state TEXT, product TEXT, company TEXT, complaint_what_happened TEXT)"
+    insert_cmd = "INSERT INTO complaint VALUES (?, ?, ?, ?, ?)"
+
+    with closing(sqlite3.connect("cfpbcomplaints.db")) as conn:
+        with closing(conn.cursor()) as cursor:
+            cursor.executescript(create_script)
+            cursor.executemany(insert_cmd, parsed)
+            conn.commit()
+
+
+def build_flow():
+    with Flow("ETL json flow") as f:
+        raw = get_complaint_data()
+        parsed = parse_complaint_data(raw)
+        store_complaints(parsed)
+
+    return f
+
+
+flow = build_flow()
+
+flow.run()
